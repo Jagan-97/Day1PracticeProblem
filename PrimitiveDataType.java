9465fd53845eea0476082da57a1ece77bcbe3b6d
Jagan-97 <jagan.psaran38@gmail.com>
2022-09-24 00:00:44 +0530

    added day1practiceproblem

---
+++ PrimitiveDataType.java
@@ -0,0 +1,20 @@
+public class PrimitiveDataType {
+    public static void main(String args[]){
+        byte b=10;
+        System.out.println("value of byte is " +b);
+        short s=4;
+        System.out.println("value of short is " +s);
+        int i=2;
+        System.out.println("value of integer is " +i);
+        long l=123456789;
+        System.out.println("value of long is " +l);
+        float f=13.25f;
+        System.out.println("value of float is" +f);
+        double d=12345.432d;
+        System.out.println("value of double is" +d);
+        boolean B=true;
+        System.out.println("value of boolean " +B);
+        char c='x';
+        System.out.println("value of char " +c);
+    }
+}
---
+++ StringEqual.java
@@ -0,0 +1,13 @@
+public class StringEqual {
+    public static void main(String args[]){
+        String str1="jagan";
+        String str2="jagan";
+        if (str1==str2)
+        {
+            System.out.println("String is equal");
+        }
+        else{
+            System.out.println("String is not equal");
+        }
+    }
+}
