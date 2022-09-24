feb53e3ff18564c291633cc48107635e2998dcc6
Jagan-97 <84405721+Jagan-97@users.noreply.github.com>
2022-09-24 09:37:22 +0530

    Update StringEqual.java

--- StringEqual.java
+++ StringEqual.java
@@ -2,7 +2,7 @@ public class StringEqual {
     public static void main(String args[]){
         String str1="jagan";
         String str2="jagan";
-        if (str1==str2)
+        if (str1.equals(str2))
         {
             System.out.println("String is equal");
         }
